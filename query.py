6242d34e7463a30714ff63607673b7861e6ff5c1
xeniyandkn <xeniya.nadkina@gmail.com>
2023-07-28 13:51:53 -0400

    add serve model configs

--- query.py
+++ query.py
@@ -1,63 +1,41 @@
-#!/usr/bin/env python3
+# import logging
+# import sys
+# import os
 
-import logging
-import os
-import sys
-from dotenv import load_dotenv
+# logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+# logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
-from langchain.llms import GPT4All
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langchain.embeddings.huggingface import HuggingFaceEmbeddings
-from llama_index import (
-    download_loader,
-    LangchainEmbedding,
-    ListIndex,
-    NotionPageReader,
-    ServiceContext,
-)
+# from llama_index import ListIndex, NotionPageReader
 
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+# integration_token = 'secret_XR6G0igcYzg4aaR0GNs9zTeHhdJsVU2WKJkiNAYgBs2'
+# print("integration_token", integration_token)
+# page_ids = ["79d4f07bdace41bba2b84002b1a847ba"]
 
-load_dotenv()
+# documents = NotionPageReader(integration_token=integration_token).load_data(
+#     page_ids=page_ids
+# )
 
-embeddings_model_name = os.environ.get("EMBEDDINGS_MODEL_NAME")
-model_path = os.environ.get("MODEL_PATH")
-model_n_ctx = os.environ.get("MODEL_N_CTX")
-model_n_batch = int(os.environ.get("MODEL_N_BATCH", 8))
-target_source_chunks = int(os.environ.get("TARGET_SOURCE_CHUNKS", 4))
+# index = ListIndex.from_documents(documents)
+# print("index is", index)
 
-# get documents
-NotionPageReader = download_loader("NotionPageReader")
-integration_token = os.getenv("NOTION_INTEGRATION_TOKEN")
-reader = NotionPageReader(integration_token=integration_token)
-page_ids = [  # testing only one page for now
-    "79d4f07bdace41bba2b84002b1a847ba"  # https://www.notion.so/alleycorpnord/How-we-work-79d4f07bdace41bba2b84002b1a847ba
-]
-documents = reader.load_data(page_ids=page_ids)
-
-# activate/deactivate the streaming StdOut callback for LLMs
-callbacks = [StreamingStdOutCallbackHandler()]
-
-# define LLM
-llm = GPT4All(
-    model=model_path,
-    n_ctx=model_n_ctx,
-    backend="gptj",
-    n_batch=model_n_batch,
-    callbacks=callbacks,
-    verbose=False,
-)
-embed_model = LangchainEmbedding(
-    HuggingFaceEmbeddings(model_name=embeddings_model_name)
-)
-service_context = ServiceContext.from_defaults(llm=llm, embed_model=embed_model)
-
-# build index
-index = ListIndex.from_documents(documents, service_context=service_context)
-
-# # get response from query
-# query = "When is payroll?"
+# # set Logging to DEBUG for more detailed outputs
 # query_engine = index.as_query_engine()
-# response = query_engine.query(query)
-# print(f"Response: {response}")
+# response = query_engine.query("when is payroll?")
+# print("response is", response)
+
+
+from llama_index import ListIndex, NotionPageReader
+
+import os
+
+
+integration_token = "secret_XR6G0igcYzg4aaR0GNs9zTeHhdJsVU2WKJkiNAYgBs2"
+page_ids = ["79d4f07bdace41bba2b84002b1a847ba"]
+reader = NotionPageReader(integration_token=integration_token)
+documents = reader.load_data(
+    page_ids=page_ids
+)
+index = ListIndex.from_documents(documents)
+
+query_engine = index.as_query_engine()
+print("query_engine is", query_engine)
